1d4cdf342b68ac9f817a7538cb06b298442b289c
Praveen Sonare <praveensonare007@gmail.com>
2025-12-24 13:52:11 +0800

    equals to develop branch

--- public/env/config.dev.js
+++ public/env/config.dev.js
@@ -1,7 +1,7 @@
 var REACT_APP_SERVER_URL='https://aecv7kyehl.execute-api.ap-southeast-2.amazonaws.com/dev';
 var REACT_APP_REGION='ap-southeast-2';
 var REACT_APP_USER_POOL_ID='ap-southeast-2_C1Dx62uIk';
-var REACT_APP_USER_POOL_APP_CLIENT_ID='5g84vdce1o893ds3d88k39tbo';
+var REACT_APP_USER_POOL_APP_CLIENT_ID='78ios3rj4i8s47c1ek2kmjf0ts';
 var REACT_APP_VERSION="$(System.TeamProject)_$(Build.SourceBranchName)_$(Build.BuildNumber)";
 var REACT_APP_WEBSOCKET="wss://ykc4kypkp9.execute-api.ap-southeast-2.amazonaws.com/dev/"
 
