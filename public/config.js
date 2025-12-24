5a995caac5db8c7a3b88c0bec1549aa2dd1d1f97
Praveen Sonare <praveensonare007@gmail.com>
2025-12-24 14:09:07 +0800

    converting to default group so it will work

--- public/config.js
+++ public/config.js
@@ -1,20 +1,16 @@
-//DEVELOPMENT
-var REACT_APP_SERVER_URL='https://aecv7kyehl.execute-api.ap-southeast-2.amazonaws.com/dev';
+var REACT_APP_SERVER_URL='https://mcm1cxutqk.execute-api.ap-southeast-2.amazonaws.com/preprod/';
 var REACT_APP_REGION='ap-southeast-2';
-var REACT_APP_USER_POOL_ID='ap-southeast-2_C1Dx62uIk';
-var REACT_APP_USER_POOL_APP_CLIENT_ID='78ios3rj4i8s47c1ek2kmjf0ts';
+var REACT_APP_USER_POOL_ID='ap-southeast-2_RO7wqUCnL';
+var REACT_APP_USER_POOL_APP_CLIENT_ID='5g84vdce1o893ds3d88k39tbo';
 var REACT_APP_VERSION="$(System.TeamProject)_$(Build.SourceBranchName)_$(Build.BuildNumber)";
-var REACT_APP_WEBSOCKET="wss://ykc4kypkp9.execute-api.ap-southeast-2.amazonaws.com/dev/"
+var REACT_APP_WEBSOCKET="wss://o0i494j4ci.execute-api.ap-southeast-2.amazonaws.com/preprod/"
 
 //TV-user
 var REACT_APP_TV_USEREMAIL="vflowtech.user@vflowtech.com"
-var REACT_APP_USER_POOL_APP_CLIENT_ID_1='4nvnbvjd81j7kr8da7hart4nb3';
+var REACT_APP_USER_POOL_APP_CLIENT_ID_1='6oeo9k2jhsskcf26b84mdkgb6m';
 
-//Staging
-// var REACT_APP_SERVER_URL='https://rldofw5t5f.execute-api.ap-southeast-2.amazonaws.com/staging/';
-// var REACT_APP_REGION='ap-southeast-2';
-// var REACT_APP_USER_POOL_ID='ap-southeast-2_aNRGXMMC5';
-// var REACT_APP_USER_POOL_APP_CLIENT_ID='2f80mn5nbkq5ddhkheuoh96abi';
-// var REACT_APP_VERSION="$(System.TeamProject)_$(Build.SourceBranchName)_$(Build.BuildNumber)"
-// var REACT_APP_WEBSOCKET="wss://h3kqh56ej8.execute-api.ap-southeast-2.amazonaws.com/staging"
-
+//API KEY
+var REACT_APP_APIKEY_google="AIzaSyAwxyff984ODfM7zn_LRo-QJgdlreW2PE4";
+//AIzaSyDT3hirHT4sh11ee_EN2SJViC5qndM-CAc   old API key for google map
+var REACT_APP_APIKEY_weather="05783ad8b8058470caf18d826cfd9d81"
+//bcda10ba323e88e96cb486015a104d1d   old API key for weather map
